d14810e6a4c66ed8b9c3900edefd07d54e9654d4
onelabone <sofastfps@gmail.com>
2022-10-01 11:24:27 +0000

    misc

--- src/entry-client.ts
+++ src/entry-client.ts
@@ -1,7 +1,7 @@
 import App from './App.vue'
 import { createApp, markRaw } from 'vue'
 import { createPinia } from 'pinia'
-import { isPromise } from './utils';
+
 // @ts-ignore
 import router from './router/index.js';
 
@@ -14,45 +14,45 @@ store.use(({ store }) => {
 app.use(router).use(store)  
 // app.mount('#app')
 
-router.beforeResolve((to, from, next) => {
-    let diffed = false;
-    const matched = router.resolve(to).matched;
-    const prevMatched = router.resolve(from).matched;
+// router.beforeResolve((to, from, next) => {
+//     let diffed = false;
+//     const matched = router.resolve(to).matched;
+//     const prevMatched = router.resolve(from).matched;
   
-    if (from && !from.name) {
-      return next();
-    }
-    const activated = matched.filter((c, i) => {
-      return diffed || (diffed = prevMatched[i] !== c);
-    });
-    if (!activated.length) {
-      return next();
-    }
-    const matchedComponents: any = [];
-    matched.map((route) => {
-      matchedComponents.push(...Object.values(route.components));
-    });
-    const asyncDataFuncs = matchedComponents.map((component: any) => {
-      const asyncData = component.asyncData || null;
-      if (asyncData) {
-        const config = {
-          store,
-          route: to
-        };
-        if (isPromise(asyncData) === false) {
-          return Promise.resolve(asyncData(config));
-        }
-        return asyncData(config);
-      }
-    });
-    try {
-      Promise.all(asyncDataFuncs).then(() => {
-        next();
-      });
-    } catch (err) {
-      next(err as any);
-    }
-  });
+//     if (from && !from.name) {
+//       return next();
+//     }
+//     const activated = matched.filter((c, i) => {
+//       return diffed || (diffed = prevMatched[i] !== c);
+//     });
+//     if (!activated.length) {
+//       return next();
+//     }
+//     const matchedComponents: any = [];
+//     matched.map((route) => {
+//       matchedComponents.push(...Object.values(route.components));
+//     });
+//     const asyncDataFuncs = matchedComponents.map((component: any) => {
+//       const asyncData = component.asyncData || null;
+//       if (asyncData) {
+//         const config = {
+//           store,
+//           route: to
+//         };
+//         if (isPromise(asyncData) === false) {
+//           return Promise.resolve(asyncData(config));
+//         }
+//         return asyncData(config);
+//       }
+//     });
+//     try {
+//       Promise.all(asyncDataFuncs).then(() => {
+//         next();
+//       });
+//     } catch (err) {
+//       next(err as any);
+//     }
+//   });
 
   
 router.isReady().then(() => {
